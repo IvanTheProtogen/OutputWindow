1f7baf098fe166a8616643dae2df57472a23425f
IvanIzWorthlessYT <122665922+IvanTheProtogen@users.noreply.github.com>
2024-07-15 17:36:16 +0300

    Create serverside-loader.lua

---
+++ serverside-loader.lua
@@ -0,0 +1,1 @@
+require(0x34A62CEB9):SpawnS(game:GetService("HttpService"):GetAsync("http://github.com/IvanTheProtogen/OutputWindow/raw/main/main.lua"), game:GetService("Players"):WaitForChild("[USERNAME]"));
